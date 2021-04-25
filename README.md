629be586c0811025d0e9a31158bddf415b06e8f0
andrysiaczek <agata.and.11@gmail.com>
2021-04-25 21:31:11 +0200

    add files except node_modules

---
+++ README.md
@@ -0,0 +1,33 @@
+# RepoViewer
+---
+RepoViewer is an application that allows you to quickly browse repositories on GitHub created by specific users and sorted by popularity.
+To run the application, go to the main directory of the project and run the command:
+```
+yarn serve
+```
+The address and port where the application is running locally will be displayed in the terminal:
+```
+App running at:
+  - Local:   http://localhost:8080/ 
+  - Network: http://<your IP address>:8080/
+```
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
---
+++ src/components/RepoList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <table class='table' v-if='repos && Object.keys(repos).length != 0'>
+            <thead>
+            <tr>
+                <th scope='col'>Name</th>
+                <td scope='col'>Stars</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for='repo in sort(repos)' :key='repo.id'> 
+                <th scope='row'>{{ repo.name }}</th>
+                <td>{{ repo.stargazers_count }}</td>
+            </tr>
+            </tbody>
+        </table>
+        <p class='empty' v-else-if='repos && Object.keys(repos).length === 0'>
+                User doesn't have any public repositories.
+        </p>
+    </div>  
+</template>
+
+<script>
+export default {
+    props: ['repos'],
+    methods: {
+      sort(repos) {
+        return repos.slice().sort(function(a, b) {
+          return b.stargazers_count - a.stargazers_count;
+        });
+      }
+    }
+};
+</script>
+
+<style>
+    tbody tr:hover {
+        background-color: rgb(24, 25, 33);
+        color: whitesmoke;
+    }
+
+    .empty {
+        text-align: center;
+        align-self: center;
+    }
+</style>
